ee58dd92422cdb547b367a4270a8ca71bc466335
fuadydheo <fuadydheo@gmail.com>
2019-11-13 13:14:44 +0700

    Initial commit

---
+++ app/src/main/java/com/imastudio/mydrumpadapps/MainActivity.java
@@ -0,0 +1,24 @@
+package com.imastudio.mydrumpadapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    // TODO 20 Deklarasi
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    // TODO 18 make method playsound 8 time without content
+
+    // TODO 19 make raw direftory in drawable and paste voice
+
+    // TODO 21 Inisialisasi
+
+    // TODO 22 Make content in method
+}
